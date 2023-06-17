851f51a24c982c7b487206dfdae5993937262063
cz295 <zkcheng1994@outlook.com>
2023-06-17 22:04:10 +0100

    homework py

---
+++ wk1_recursion/hw1.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    pass
